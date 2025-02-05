8eb9a1515882d5791217b28b7d82a3021604ffeb
root <root@ip-172-31-47-81.ap-south-1.compute.internal>
2025-02-05 17:16:00 +0000

    state

--- development/main.tf
+++ development/main.tf
@@ -1,6 +1,7 @@
-module "instance"
- source = "../modules" # Path to your module repository
+module "instance" {
+  source = "../modules" # Path to your module repository
 
- ami_id = "ami-00bb6a80f01f03502"
- instance_type = "t2.medium"
- instance_name = "prathesh"
+  ami_id        = "ami-00bb6a80f01f03502"
+  instance_type = "t2.medium"
+  instance_name = "prathesh"
+}
