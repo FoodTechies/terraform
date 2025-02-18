7a6fd530fd6d30406ffed9be015ed686a8a0b014
Wasim Akram <wasim.akram@mercedes-benz.com>
2025-02-18 14:58:50 +0100

    bucket_name

--- development/main.tf
+++ development/main.tf
@@ -4,7 +4,7 @@ module "instance" {
   ami_id        = "ami-00bb6a80f01f03502"
   instance_type = "t2.medium"
   instance_name = "darshan"
-  bucket_name   = "darshan"
+  bucket_name   = "darshan_123"
   env           = "dev"
   username      = "darshan"
 }
