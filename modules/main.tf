682c30d9c9ba3cabbc0bc0ec8ccd71554bd0c177
Wasim Akram <wasim.akram@mercedes-benz.com>
2025-02-18 14:41:29 +0100

    added dev and staging

--- modules/main.tf
+++ modules/main.tf
@@ -6,3 +6,20 @@ resource "aws_instance" "web" {
     Name = "var.instance_name"
   }
 }
+
+resource "aws_s3_bucket" "finance" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.env
+  }
+}
+
+resource "aws_iam_user" "lb" {
+  name = var.username
+
+  tags = {
+    tag-key = var.username
+  }
+}
---
+++ production/main.tf
@@ -0,0 +1,8 @@
+module "instance" {
+  source = "../modules" # Path to your module repository
+
+  ami_id        = "ami-00bb6a80f01f03502"
+  instance_type = "t2.medium"
+  instance_name = "prathesh"
+}
+
