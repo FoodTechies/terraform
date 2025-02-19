670440804789d0b10500f8a998a090ddf7ace6b1
Wasim Akram <wasim.akram@mercedes-benz.com>
2025-02-19 16:30:25 +0100

    updated the code

--- development/eks.tf
+++ development/eks.tf
@@ -4,7 +4,7 @@ module "eks" {
   cluster_name    = "eks-cluster"
   cluster_version = "1.31"
   vpc_id          = "vpc-0da81cc0946988e33"
-  subnet_ids      = ["subnet-06360934911bb8b90", "subnet-059dd15f3513f32b4", subnet-0cbe70e562afd3b7b]
+  subnet_ids      = ["subnet-06360934911bb8b90", "subnet-059dd15f3513f32b4", "subnet-0cbe70e562afd3b7b"]
   instance_types  = ["t2.medium"]
   ami_id          = "ami-000ac001d85d66652"
   min_capacity    = 1
